b2ae26d70032130b43d30dc1dac6c6cf651884d6
Marcel Fonseca <marcelsf23br@gmail.com>
2018-11-14 16:53:20 +0100

    Works fine, looks horrible

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+Restaurant.create(name: 'Tim Raue', address: 'Berlin', category: 'chinese')
+Restaurant.create(name: 'Dom', address: 'Sao Paulo', category: 'italian')
+Restaurant.create(name: 'Asterix', address: 'Normandy', category: 'french')
+Restaurant.create(name: 'Red Dragon', address: 'Tokyo', category: 'japanese')
+Restaurant.create(name: 'The Celt', address: 'Brussels', category: 'belgian')
+Restaurant.create(name: 'The Godfather', address: 'Sicily', category: 'italian')
